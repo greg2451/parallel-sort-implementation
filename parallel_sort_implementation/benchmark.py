50a67397067d732685027313218a91df9efb64d8
greg2451 <51173502+greg2451@users.noreply.github.com>
2023-05-18 23:34:38 +0200

    save plots + better points

--- parallel_sort_implementation/benchmark.py
+++ parallel_sort_implementation/benchmark.py
@@ -7,6 +7,6 @@ from parallel_sort_implementation.python.test_performance import (
 if __name__ == "__main__":
     test_single_perf(input_size=10000)
 
-    input_sizes = [i for i in range(100, 10000, 1000)]
+    input_sizes = [i**2 for i in range(1, 2000, 100)]
     print(input_sizes)
-    plot_perf(input_sizes=input_sizes, include_threaded=True)
+    plot_perf(input_sizes=input_sizes, include_multithreaded_and_multiprocessed=True)
--- parallel_sort_implementation/python/test_performance.py
+++ parallel_sort_implementation/python/test_performance.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from pathlib import Path
+from datetime import datetime
 
 from parallel_sort_implementation.python import (
     quick_sort,
@@ -9,8 +11,11 @@ from parallel_sort_implementation.python import (
 from parallel_sort_implementation.python.utils import test_sort
 from parallel_sort_implementation.cython import quicksort_cpp
 
+SAVE_DIR = Path(__file__).parent.parent / "results"
+SAVE_DIR.mkdir(exist_ok=True)
 
-def plot_perf(input_sizes: list, include_threaded: bool):
+
+def plot_perf(input_sizes: list, include_multithreaded_and_multiprocessed: bool):
     gold_standard_times = []
     regular_quicksort_times = []
 
@@ -21,29 +26,34 @@ def plot_perf(input_sizes: list, include_threaded: bool):
     for input_size in tqdm(input_sizes):
         gold_standard_time = test_sort(sorted, input_size)
         regular_quicksort_time = test_sort(quick_sort, input_size)
-        if include_threaded:
+        if include_multithreaded_and_multiprocessed:
             multithreaded_quicksort_time = test_sort(quick_sort_th, input_size)
-        multiprocess_quicksort_time = test_sort(quick_sort_mp, input_size)
+            multiprocess_quicksort_time = test_sort(quick_sort_mp, input_size)
         cpp_quicksort_time = test_sort(quicksort_cpp, input_size)
 
         gold_standard_times.append(gold_standard_time)
         regular_quicksort_times.append(regular_quicksort_time)
-        if include_threaded:
+        if include_multithreaded_and_multiprocessed:
             multithreaded_quicksort_times.append(multithreaded_quicksort_time)
-        multiprocess_quicksort_times.append(multiprocess_quicksort_time)
+            multiprocess_quicksort_times.append(multiprocess_quicksort_time)
         cpp_quicksort_times.append(cpp_quicksort_time)
 
     plt.plot(input_sizes, gold_standard_times, label="Gold Standard")
     plt.plot(input_sizes, regular_quicksort_times, label="Regular Quicksort")
-    if include_threaded:
+    if include_multithreaded_and_multiprocessed:
         plt.plot(
             input_sizes, multithreaded_quicksort_times, label="Multithreaded Quicksort"
         )
-    plt.plot(input_sizes, multiprocess_quicksort_times, label="Multiprocess Quicksort")
+        plt.plot(
+            input_sizes, multiprocess_quicksort_times, label="Multiprocess Quicksort"
+        )
     plt.plot(input_sizes, cpp_quicksort_times, label="C++ Quicksort")
     plt.xlabel("Input Size")
     plt.ylabel("Execution")
     plt.legend(loc="upper left")
+    plt.savefig(
+        SAVE_DIR / f"perf_plot_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png"
+    )
     plt.show()
 
 
